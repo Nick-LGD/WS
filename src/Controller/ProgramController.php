055246254e5853f8284122f096e6ba9f17cff2a2
Nicolas <n.legrand77190@gmail.com>
2021-10-13 12:29:31 +0200

    add bdd + controller + view

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
+
+class ProgramController extends AbstractController
+{
+    #[Route('/program', name: 'program_index')]
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+             ->getRepository(Program::class)
+             ->findAll();
+
+         return $this->render(
+             'program/index.html.twig',
+             ['programs' => $programs]
+         );
+    }
+
+    #[Route('/program/{id}', methods:['GET'] , name: 'program_show')]
+    public function show(int $id): Response
+    {
+        $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findOneBy(['id' => $id]);
+
+    if (!$program) {
+        throw $this->createNotFoundException(
+            'No program with id : '.$id.' found in program\'s table.'
+        );
+    }
+    return $this->render('program/show.html.twig', [
+        'program' => $program,
+    ]);
+    }
+}

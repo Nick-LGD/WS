b7e82bbca1e3e667adaaaa782e3e0e6251c13151
Nicolas <n.legrand77190@gmail.com>
2021-10-14 11:07:25 +0200

    update bdd

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -3,41 +3,101 @@
 namespace App\Entity;
 
 use App\Repository\ProgramRepository;
-use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title")
  */
 class Program
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank(message="ne me laisse pas tout vide")
+     * @Assert\Length(max="255",  maxMessage="La catégorie saisie {{ value }} est trop longue, elle ne devrait pas dépasser {{ limit }} caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="ne me laisse pas tout vide")
+     * @Assert\Regex(pattern="/plus belle la vie/", match=false, message="On parle de vraies séries ici")
      */
-    private $summary;
+    private $synopsis;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $poster;
 
-   /**
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Season", mappedBy="program")
+     */
+    private $seasons;
+
+    public function __construct()
+    {
+        $this->seasons = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection|Season[]
+     */
+    public function getSeasons(): Collection
+    {
+        return $this->seasons;
+    }
+
+    /**
+     * @param Season $season
+     * @return Program
+     */
+    public function addSeason(Season $season): self
+    {
+        if (!$this->seasons->contains($season)) {
+            $this->seasons[] = $season;
+            $season->setProgram($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Season $season
+     * @return Program
+     */
+
+    public function removeSeason(Season $season): self
+    {
+        if ($this->seasons->contains($season)) {
+            $this->seasons->removeElement($season);
+            // set the owning side to null (unless already changed)
+            if ($season->getProgram() === $this) {
+                $season->setProgram(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
+
 
     public function getId(): ?int
     {
@@ -56,14 +116,14 @@ class Program
         return $this;
     }
 
-    public function getSummary(): ?string
+    public function getSynopsis(): ?string
     {
-        return $this->summary;
+        return $this->synopsis;
     }
 
-    public function setSummary(string $summary): self
+    public function setSynopsis(string $synopsis): self
     {
-        $this->summary = $summary;
+        $this->synopsis = $synopsis;
 
         return $this;
     }
@@ -91,4 +151,5 @@ class Program
 
         return $this;
     }
+
 }
